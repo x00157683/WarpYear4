9a6833daf56aec8b81f73f2112a59921d2c3c2e1
Darren McLean <deemclean46@gmail.com>
2022-02-13 13:34:28 +0000

    Added HomePage and Model

---
+++ Shared/Models/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        [Required]
+        public string Make { get; set; }
+        
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public double Range { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }  
+
+
+    }
+}
---
+++ Shared/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        public List<Car> Cars { get; set; }
+
+
+
+    }
+
+    
+}
