a726f1d6a4bd152bf19fba63d2f6b754f1369b64
Darren McLean <deemclean46@gmail.com>
2022-02-16 17:13:59 +0000

    Added Models - Booking, License, User

---
+++ Shared/Models/Booking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Booking
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime TimeRemain { get; set; }
+
+        [Required]
+        public DateTime TimeElapsed { get; set; }
+
+        [Required]
+        public double Cost { get; set; }
+
+        [Required]
+        public Car Cars { get; set; }
+
+
+    }
+}
---
+++ Shared/Models/License.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+    public class License
+    {
+
+        [Required]
+        public string FName { get; set; }
+
+        [Required]
+        public string LName { get; set; }
+
+        [Required]
+        public DateTime dob { get; set; }
+
+        public int YearsHeld { get; set; }
+
+
+
+    }
+}
