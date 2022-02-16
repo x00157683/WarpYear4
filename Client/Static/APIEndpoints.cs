afe52f8c5d4396b21d5e16f933880de348f7f051
Darren McLean <deemclean46@gmail.com>
2022-02-16 15:59:42 +0000

    updating client publish details

--- Client/Static/APIEndpoints.cs
+++ Client/Static/APIEndpoints.cs
@@ -4,11 +4,11 @@
     {
 
 #if DEBUG
-        // do 
+      
         internal const string SererBaseUrl = "https://localhost:5003";
 
 #else
-        live
+        
         internal const string SererBaseUrl = "https://year4pipev5.azurewebsites.net/index.html";
 
 #endif
