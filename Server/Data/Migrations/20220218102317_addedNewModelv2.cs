c0ba1d713feef8e9d2cc009695a8895b7eacd56f
Darren McLean <deemclean46@gmail.com>
2022-02-18 10:24:53 +0000

    Db update

---
+++ Server/Data/Migrations/20220218102317_addedNewModelv2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Data.Migrations
+{
+    public partial class addedNewModelv2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StartTime",
+                value: new DateTime(2022, 2, 18, 10, 23, 16, 572, DateTimeKind.Local).AddTicks(8404));
+
+            migrationBuilder.UpdateData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StartTime",
+                value: new DateTime(2022, 2, 18, 10, 23, 16, 572, DateTimeKind.Local).AddTicks(8500));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "StartTime",
+                value: new DateTime(2022, 2, 17, 21, 12, 11, 350, DateTimeKind.Local).AddTicks(467));
+
+            migrationBuilder.UpdateData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StartTime",
+                value: new DateTime(2022, 2, 17, 21, 12, 11, 350, DateTimeKind.Local).AddTicks(535));
+        }
+    }
+}
